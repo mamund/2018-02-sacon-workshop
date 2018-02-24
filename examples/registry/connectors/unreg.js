ed5e6232e834132c6e5f0c06fff340a03d5e7f46
Mike Amundsen <mamund@yahoo.com>
2018-02-24 18:32:10 -0500

    add unreg support

--- examples/registry/connectors/unreg.js
+++ examples/registry/connectors/unreg.js
@@ -6,6 +6,9 @@
 
 // handles HTTP resource operations 
 var wstl = require('./../wstl.js');
+var utils = require('./utils.js');
+var registry = require('./../components/registry.js');
+
 var gTitle = "DISCO Registry";
 
 module.exports = main;
@@ -16,9 +19,79 @@ function main(req, res, parts, respond) {
   case 'GET':
     sendPage(req, res, respond);
     break;
+  case 'POST':
+    postRemove(req, res, respond);
+    break;  
+  case 'DELETE':
+    deleteRemove(req, res, respond);
+    break;
   default:
     respond(req, res, utils.errorResponse(req, res, 'Method Not Allowed', 405));
     break;
+  }
+}
+
+function postRemove(req, res, respond) {
+  var body, doc, msg;
+
+  body = '';
+  
+  // collect body
+  req.on('data', function(chunk) {
+    body += chunk;
+  });
+
+  // process body
+  req.on('end', function() {
+    try {
+      msg = utils.parseBody(body, req.headers["content-type"]);
+      console.log(msg);
+      doc = registry('remove', msg.registryID);
+      if(doc && doc.type==='error') {
+        doc = utils.errorResponse(req, res, doc.message, doc.code);
+      }
+    } 
+    catch (ex) {
+      doc = utils.errorResponse(req, res, 'Server Error', 500);
+    }
+
+    if (!doc) {
+      respond(req, res, {code:301, doc:"", 
+        headers:{'location':'//'+req.headers.host+"/"}
+      });
+    } 
+    else {
+      respond(req, res, {code:301, doc:"", 
+        headers:{'location':'//'+req.headers.host+"/"}
+      });
+    }
+  });
+}
+
+function deleteRemove(req, res, respond) {
+  var id, doc, args;
+
+  args = utils.getQArgs(req);
+  try {
+    id = args['registryID'];
+    doc = registry('remove', id);
+    if(doc && doc.type==='error') {
+      doc = utils.errorResponse(req, res, doc.message, doc.code);
+    }
+  } 
+  catch (ex) {
+    doc = utils.errorResponse(req, res, 'Server Error', 500);
+  }
+
+  if (!doc) {
+    respond(req, res, {code:301, doc:"", 
+      headers:{'location':'//'+req.headers.host+"/"}
+    });
+  } 
+  else {
+    respond(req, res, {code:301, doc:"", 
+      headers:{'location':'//'+req.headers.host+"/"}
+    });
   }
 }
 
